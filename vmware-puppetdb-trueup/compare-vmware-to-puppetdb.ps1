50dd6365c18d1f4c2aaac50687a9ae6d419e609f
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-12-23 17:03:00 -0500

    Add compare-vmware-to-puppetdb script
    This scirpt is still a work in progress but is useful as-is. Currently
    it will report on nodes in a VMware cluster after looking for each one
    in PuppetDB.
    
    Co-Authored-By: Rob Nelson <rnelson0@gmail.com>

---
+++ vmware-puppetdb-trueup/compare-vmware-to-puppetdb.ps1
@@ -0,0 +1,72 @@
+param (
+    [String]$VirtualCenter = "vcenter-prod1.ops.puppetlabs.net",
+    [String]$VMwareUserName,
+    [SecureString]$VMwarePassword,
+    [String]$VMwareCluster = 'operations2',
+    [String]$PuppetServer = 'https://puppet.ops.puppetlabs.net',
+    [String]$Token = $(Get-Content -Path "$HOME/.puppetlabs/token" -ErrorAction SilentlyContinue)
+)
+
+if ($PuppetServer -match 'https://') {
+    $port = 8081
+    $SkipCertificateCheck = $true
+} else {
+    $port = 8080
+    $SkipCertificateCheck = $false
+}
+$pdb_api = "${PuppetServer}:${port}/pdb/query/v4"
+
+$headers = @{
+    'Content-Type' = 'application/json'
+}
+if ($Token) {
+    $headers += @{'X-Authentication' = ${Token}}
+}
+
+if ($VMwareUserName -and $VMwarePassword) {
+    $creds = New-Object System.Management.Automation.PSCredential($VMwareUserName, $VMwarePassword)
+} elseif ($VMwareUserName) {
+    $creds = Get-Credential -Message "Enter your vCenter credentials" -UserName "$($VMwareUserName)"
+} else {
+    $creds = Get-Credential -Message "Enter your vCenter credentials"
+}
+Connect-VIServer -Server $VirtualCenter -Credential $creds |Out-Null
+
+$VMS = Get-Cluster -Name $VMwareCluster | Get-VM | Where-Object { $_.PowerState -eq 'PoweredOn' } 
+
+$result_objects = @()
+
+foreach ($vm in $VMS) {
+    $nic = $vm | Get-NetworkAdapter | Select-Object -First 1
+
+    $result_object = [PSCustomObject]@{
+        VMName                = $vm.Name
+        IPAddress             = $($vm.guest.IPAddress | Where-Object { $_.StartsWith('10') } | Select-Object -First 1)
+        MacAddress            = $nic.MacAddress
+        VMNetworkName         = $nic.NetworkName
+        VMwareCluster         = $VMwareCluster
+        FoundInPuppetDB       = $false
+        PuppetFQDN            = $null
+        PuppetReportTimestamp = $null
+    }
+
+    $mac_query = @{query = "inventory[certname]{ facts.networking.mac = '$($nic.MacAddress)' order by certname }" } | ConvertTo-Json
+    if ($SkipCertificateCheck) {
+        $mac_results = Invoke-RestMethod -Method Post -Uri ${pdb_api} -Headers $headers -Body $mac_query -SkipCertificateCheck
+    } else {
+        $mac_results = Invoke-RestMethod -Method Post -Uri ${pdb_api} -Headers $headers -Body $mac_query
+    }
+    
+    if ($mac_results.length -gt 0) {
+        $result_object.FoundInPuppetDB = $true
+        $result_object.PuppetFQDN = $mac_results.certname
+
+        $report_query = @{query = "nodes{ certname = '$($result_object.PuppetFQDN)' }" } | ConvertTo-Json
+        $report_results = Invoke-RestMethod -Method Post -Uri ${pdb_api} -Headers $headers -Body $report_query -SkipCertificateCheck
+        $result_object.PuppetReportTimestamp = $report_results.report_timestamp
+    }
+
+    $result_objects += $result_object
+}
+
+$result_objects
